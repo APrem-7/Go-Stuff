cfd2ddeefbc244abccbe10a582114b84db4d2093
APrem-7 <ap7leo@gmail.com>
2026-02-26 21:51:41 +0530

    added

--- chickenchannel.go
+++ chickenchannel.go
@@ -7,15 +7,19 @@ import (
 )
 
 var MAX_PAVBHAJI_PRICE float32 = 10
+var MAX_PURI_PRICE float32 = 10
 
 func main() {
 	var pavBhaji = make(chan string)
+	var puri = make(chan string)
 	var websites = []string{"walmart.com", "costco.com", "walgreens.com"}
 	for i := range websites {
 		go getChickenPrices(websites[i], pavBhaji)
+		go getPuriPrice(websites[i],puri)
 	}
 
 	sendMessage(pavBhaji)
+	sendMessage(puri)
 
 }
 
@@ -30,6 +34,16 @@ func getChickenPrices(websites string, pavBhaji chan string) {
 
 	}
 
+}
+
+func getPuriPrice(websites string,puri chan string){
+	for{
+		time.Sleep(time.Second * 1)
+		puriPrice := rand.Float32()*20
+		if puriPrice <= MAX_PURI_PRICE{
+			puri <- websites
+		}
+	}
 }
 
 func sendMessage(pavBhaji chan string) {
