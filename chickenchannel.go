99126e565e046f976eb86df2b613ea6dd5117994
APrem-7 <ap7leo@gmail.com>
2026-02-26 21:43:14 +0530

    after

--- chickenchannel.go
+++ chickenchannel.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
-	"sync"
+	"math/rand"
 	"time"
 )
-	
-var MAX_PAVBHAJI_PRICEfloat32=10
+
+var MAX_PAVBHAJI_PRICE float32 = 10
 
 func main() {
-	var pavBhaji= make(chan string)
-	var websites=[]string{"walmart.com","costco.com","walgreens.com"}
-	for i:= range websites{
-		getChickenPrices(i,pavBhaji)
+	var pavBhaji = make(chan string)
+	var websites = []string{"walmart.com", "costco.com", "walgreens.com"}
+	for i := range websites {
+		go getChickenPrices(websites[i], pavBhaji)
 	}
 
-	
+	sendMessage(pavBhaji)
+
 }
 
-func getChickenPrices(websties string,pavBhaji chan string){
+func getChickenPrices(websites string, pavBhaji chan string) {
 	for {
-		time.Sleep(time.Seconds*1)
-		pavBhajiPrice=rand.Float32()*20
-		if pavBhajiPrice<=MAX_PAVBHAJI_PRICEfloat32{
-			chickenChannel <- websites
+		time.Sleep(time.Second * 1)
+		pavBhajiPrice := rand.Float32() * 20
+		if pavBhajiPrice <= MAX_PAVBHAJI_PRICE {
+			pavBhaji <- websites
 			break
 		}
 
@@ -32,6 +32,6 @@ func getChickenPrices(websties string,pavBhaji chan string){
 
 }
 
-func sendMessage(chickenChannel chan string){
-	fmt.Printf("Found a deal on chicken at %s",<- chickenChannel)
+func sendMessage(pavBhaji chan string) {
+	fmt.Printf("Found a deal on chicken at %s", <-pavBhaji)
 }
